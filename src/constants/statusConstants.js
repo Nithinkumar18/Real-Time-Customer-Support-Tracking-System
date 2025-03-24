faa27bfc3f5347e00d3c5103a932408ab5abbe39
Nithin <vooturinithinkumar@gmail.com>
2025-03-24 16:32:17 +0530

    add constants

---
+++ src/constants/statusConstants.js
@@ -0,0 +1,10 @@
+export const constants = {
+    SUCCESS: 200,
+    INTERNAL_SERVER_ERROR: 500,
+    CREATED: 201,
+    FORBIDDEN: 403,
+    NOT_FOUND: 404,
+    UNAUTHORIZED: 401,
+    BAD_REQUEST: 400
+}
+
