7eb5faa1a660d49c0a64c3bcde93e9a04fc8cb06
Nithin <vooturinithinkumar@gmail.com>
2025-04-02 17:12:55 +0530

    define middleware for authenticating and authorizing user role

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -3,6 +3,9 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const logger = require('../loggers/logger');
 const constants = require('../constants/statusConstants');
+const responseInfo = require('../constants/responseMessages');
+const userService = require('../service/userService');
+const mysession = require('../config/cacheService');
 
 async function authenticate(req,res,next){
     try{
@@ -10,18 +13,20 @@ async function authenticate(req,res,next){
         const extractedToken = req.headers.authorization;
         if(!extractedToken || !extractedToken.startsWith('Bearer')){
             logger.error(`No token found / Invalid token format ${constants.BAD_REQUEST}`);
-            return res.status(constants.BAD_REQUEST).json({message:"Please login and try again"});
+            return res.status(constants.BAD_REQUEST).json({message:responseInfo.TOKEN_UNDEFINED});
         }
 
         const token = extractedToken.split(' ')[1];
         const _validateToken =  jwt.verify(token,process.env.SECRET);
         req.role = _validateToken.role;
+        req.email = _validateToken.email;
         logger.info(`authenticating user ${req.url}`);
         next();
  }
     catch(err){
+        mysession.clearSession("userSession");
         logger.error(`Error authenticating user: ${req.url}`,err);
-        return res.status(constants.INTERNAL_SERVER_ERROR).json({message: 'Authenticating user failed',err:err.message});
+        return res.status(constants.INTERNAL_SERVER_ERROR).json({message: responseInfo.TOKEN_EXPIRED,err:err.message});
     }
 }
 
--- src/middleware/authrorizeUser.js
+++ src/middleware/authrorizeUser.js
@@ -1,24 +1,25 @@
 
 const logger = require('../loggers/logger');
 const constants = require('../constants/statusConstants');
+const responseInfo = require('../constants/responseMessages');
 
 
 const authorizeUser = (validRoles) => {
     return (req,res,next) => {
         try{
           const role = req.role;
-          const _authorizedUser = validRoles.contains(role);
+          const _authorizedUser = validRoles.includes(role);
           if(_authorizedUser){
-            logger.info(`validated user role for request: ${req.URL}`);
+            logger.info(`validated user role for request: ${req.path}`);
             next();
           }
           else{
-            logger.info(`User not authorized to perform this operation:${req.URL}`);
-            return res.status(constants.UNAUTHORIZED).json({message: 'you are not allowed to perform this action'});
+            logger.info(`User not authorized to perform this operation:${req.path}`);
+            return res.status(constants.UNAUTHORIZED).json({Access_Denied: responseInfo.ACCESS_DENIED});
           }
         }
         catch(err){
-           logger.error(`Error validating user role ${req.URL}`);
+           logger.error(`Error validating user role ${req.path}`,err);
            return res.status(constants.INTERNAL_SERVER_ERROR).json({message: 'Error validating user role',err: err.message});
         }
     }
